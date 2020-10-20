bdd0312bd2f1e04ee0ef0de11615cdfcfa4abfc7
vagmithra123 <vagmithra@gmail.com>
2020-10-20 14:38:41 -0700

    Worked on Python numbers data type

---
+++ numbers.py
@@ -0,0 +1,14 @@
+print(f'Sum of 2 and 3 is {2 + 3}')
+
+print(f'1 subtracted from 2 is  {2-1}')
+
+print(f'2 times 4 is {2 * 4}')
+
+print(f'2 divided by 2 is {2/2}')
+
+print(f'2 mod 2 is {2 % 2}')
+
+print(f'Quotient of 2 divided 2 is {2 // 2}')
+
+
+ 
