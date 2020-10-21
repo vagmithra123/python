fe9f15242ec28ff56a713c04e738902dc9f921f6
vagmithra123 <vagmithra@gmail.com>
2020-10-21 14:29:03 -0700

    Adding Pyhton Strings and varibale assignment files

---
+++ PythonStrings.py
@@ -0,0 +1,13 @@
+print('hello')
+
+print("Hello")
+
+print("I'm going on a run")
+
+print("hello \n world")
+
+print('hello \t world')
+
+print(len('hello'))
+
+print(len('I am cool'))
---
+++ VariableAssignment.py
@@ -0,0 +1,13 @@
+a = 'hello'
+print(a)
+
+a = 10
+print(a)
+
+for i in range(2,6):
+  a = a + a
+  print(a)
+
+print(type(a))
+
+
