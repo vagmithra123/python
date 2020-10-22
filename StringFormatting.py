cda815d466676e3e30ff8a08b0c5a2a9abf146dc
vagmithra123 <vagmithra@gmail.com>
2020-10-22 14:07:38 -0700

    Adding String Formatting methods

---
+++ StringFormatting.py
@@ -0,0 +1,15 @@
+print('This is a string {}'.format('INSERTED'))
+
+print('This {} {} {}'.format('is', 'a','String'))
+
+print('This {q} {r} {l} '.format(q= 'is', r = 'a', l ='string'))
+
+print('Hey {3} {0} {1} {2}'.format('little','bo', 'peep', 'hello'))
+
+result = 100/777
+
+print("The result was {r:1.5f}".format(r=result))
+
+name = "Sam"
+age = 3
+print(f'Hello, his name is {name} and age is {age}')
