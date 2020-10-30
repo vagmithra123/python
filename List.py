d3503db3862d777cf1e737f571f65eb961190839
vagmithra123 <vagmithra@gmail.com>
2020-10-30 15:12:44 -0700

    Adding Dictionary and List files

---
+++ List.py
@@ -0,0 +1,16 @@
+mylist = [1, 2,3]
+
+print(mylist)
+
+print(f'Elements of list are {mylist}')
+
+print(f'Elements of list from index 1 are {mylist[1:]}')
+
+newlist = [4,5]
+
+print(f'Contatenating mylist and newlist {mylist + newlist}')
+
+
+mylist[1] = 4
+
+print(mylist)
