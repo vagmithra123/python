309b848659a96f8b7e1abc3d56531cc777249dfb
vagmithra123 <vagmithra@gmail.com>
2020-11-01 03:05:28 -0800

    Amended lists file

--- List.py
+++ List.py
@@ -14,3 +14,40 @@ print(f'Contatenating mylist and newlist {mylist + newlist}')
 mylist[1] = 4
 
 print(mylist)
+
+#append
+#pop
+#sort
+#reverse
+
+print(f'List before appending 5 {mylist}')
+
+mylist.append(5)
+
+print(f'List after appending 5 {mylist}')
+
+print(f'List before pop {mylist}')
+
+mylist.pop()
+
+print(f'List after popping {mylist}')
+
+mylist.pop(1)
+
+print(f'List after popping index 1 {mylist}')
+
+mylist.append(6)
+
+mylist.append(4)
+
+print(f'List before sorting {mylist}')
+
+mylist.sort()
+
+print(f'List after sorting {mylist}')
+
+print(f'List before reversing {mylist}')
+
+mylist.reverse()
+
+print(f'List after reversing {mylist}')
