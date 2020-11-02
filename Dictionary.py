e5f7da93e4d2184f18d56dd4fa60889ff585c7da
vagmithra123 <vagmithra@gmail.com>
2020-11-02 10:30:58 -0800

    Adding Dictionary methods

---
+++ Dictionary.py
@@ -0,0 +1,27 @@
+prices = {'apple' : 2.99, 'orange' : 3.99, 'milk' : 5.99, 'spinach' : 4.99}
+
+
+
+print(f'Price of apple is {prices["apple"]}')
+
+
+d = {'k1' : 1 , 'k2' : [1,2,3], 'k3' : {'insidekey':'v1'}}
+
+print(f'Value of k1 is {d["k1"]}')
+
+print(f'Value of k2 is {d["k2"]}')
+
+print(f'Value of k3 is {d["k3"]["insidekey"]}')
+
+print(f'Value of k2 at index 1 is {d["k2"][1]}')
+
+print(f'Dictionary is {d}')
+
+print(f'All keys of d {d.keys()}')
+
+print(f'All values of d {d.values()}')
+
+print(f'All items of d {d.items()}')
+
+
+
