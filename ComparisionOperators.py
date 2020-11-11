d2b7a30bca3cf37b321f7ab5a9a537cc01bf76b8
vagmithra123 <vagmithra@gmail.com>
2020-11-11 02:08:38 -0800

    Adding Comparision operators

---
+++ ComparisionOperators.py
@@ -0,0 +1,26 @@
+print(f'2 is equal to 2 is  {2 == 2}')
+
+print(f'2 is equal to 1 is {2 ==1}')
+
+print(f'hello is equal to bye is {"hello" == "bye"}')
+
+print(3 != 3)
+
+print(f'Is 2 greater than 5 {2 > 5}')
+
+print(f'Is 2 greater than or equal to 2 {2 >=2}')
+
+#operator chaining
+print(f'2 less than 3 and 3 less than 5 {2 <3 <5}')
+
+#and
+print(f'2 less than 3 and 3 less than 5 {2 <3 and  3<5} ')
+
+#or
+print(f'5 is equal to 5 or 6 is equal to 7 {5 == 5 or 6 == 7}')
+
+#not
+print(f'1 not equal to 1 { 1 != 1}')
+
+
+
