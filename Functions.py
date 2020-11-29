bd214f55629965abe0ae230365644e3159c1786e
vagmithra123 <vagmithra@gmail.com>
2020-11-28 17:41:25 -0800

    Adding Functions

--- Functions.py
+++ Functions.py
@@ -3,8 +3,16 @@
 Docstring explains function.
 '''
 
-def say_hello(name):
+def say_hello(name='Default'):
 	print("Hello " + name)
 	
 
 say_hello("sudha")
+say_hello()
+say_hello()
+
+def add_num(num1,num2):
+	return num1+num2
+
+result = add_num(5,6)
+print(result)
