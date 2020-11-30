981eeead1a385619e5f9eff97cc2581dd1b9704e
vagmithra123 <vagmithra@gmail.com>
2020-11-30 13:32:42 -0800

    Adding Functions

--- Functions.py
+++ Functions.py
@@ -9,7 +9,6 @@ def say_hello(name='Default'):
 
 say_hello("sudha")
 say_hello()
-say_hello()
 
 def add_num(num1,num2):
 	return num1+num2
@@ -20,6 +19,7 @@ print(result)
 str = add_num('10','20')
 print(str)
 
+#Using Logical statements like if, for, while in functions
 #check a single number is even
 
 def check_even(num):
@@ -31,3 +31,40 @@ def check_even(num):
 
 ans = check_even(24)
 print(ans)
+
+# passing a list to a method
+def check_even_list(num_list):
+	for num in num_list:
+		if num % 2 == 0:
+			return True
+		else:
+			pass
+
+	return False
+
+ans = check_even_list([1,3,5])
+print(ans)
+
+ans = check_even_list([2,4,6])
+print(ans)
+
+
+#return a list of even number in given list
+
+def return_even_list(num_list):
+
+	even_numbers = []
+
+	for num in num_list:
+		if num % 2 == 0:
+			even_numbers.append(num)
+
+		else:
+			pass
+
+	return even_numbers
+
+
+list = return_even_list([1,2,3,4,5,6])
+print(list)
+
