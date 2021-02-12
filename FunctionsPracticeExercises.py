7ad95a16c7dda84b137b0891b1bb1e9a13c26f22
vagmithra123 <vagmithra@gmail.com>
2021-02-11 17:16:58 -0800

    Modified Practice exercises

--- FunctionsPracticeExercises.py
+++ FunctionsPracticeExercises.py
@@ -70,7 +70,28 @@ def has_33(nums):
 
 	return False
 
+''' 
+PAPER DOLL: Given a string, return a string where for every character in the original there are three characters
+paper_doll('Hello') --> 'HHHeeellllllooo'
+paper_doll('Mississippi') --> 'MMMiiissssssiiippppppiii'
+'''
 
-			 
+def paper_doll(text):
+	return (text[0: len(text)] * 3)
+
+'''
+BLACKJACK: Given three integers between 1 and 11, if their sum is less than or equal to 21, return their sum. If their sum exceeds 21 and there's an eleven, reduce the total sum by 10. Finally, if the sum (even after adjustment) exceeds 21, return 'BUST'
+blackjack(5,6,7) --> 18
+blackjack(9,9,9) --> 'BUST'
+blackjack(9,9,11) --> 19
+'''
+
+def blackjack(a,b,c):
+	if sum(a,b,c) <= 21:
+		return sum((a,,b,c))
+	elif sum ((a,b,c)) >= 21 and 11 in (a,b,c):
+		return sum((a,,b,c)) - 10
+	else:
+		return 'BUST'
 
 
